31a9ce84c239dbd96a55f202cd803675db86d2b3
Micah Morris <micah.morris42.com>
2023-02-01 20:31:44 -0700

    added new var to store choice

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+//computer chooses rock paper or scissors
 function getComputerChoice(choice) {
     return choice [Math.floor(Math.random() * choice.length)]
     
 }
 var choice = ['Rock', 'Paper', 'Scissors'];
-console.log(getComputerChoice (choice));
+var computerChoice = getComputerChoice (choice)
+console.log(computerChoice);
+
+
+let playerChoice = prompt('Rock, Paper, or Scissors?')
+
+if (computerChoice ==  playerChoice) {
+    console.log('A Draw!');
+}
+
